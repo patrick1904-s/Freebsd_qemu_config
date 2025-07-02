2a3c4015689691ad5b9349e4ca9b5913bf2f62fb
patrick1904-s <shanecardoz26@gmail.com>
2025-07-02 21:12:05 +0530

    Modification in the reg_open.sh >>> bootup.sh

--- bootup.sh
+++ bootup.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-DISK_FILE="freebsd.qcow2"
-RAM="4096"
-CPUS="4"
-
 qemu-system-x86_64 \
   -enable-kvm \
-  -m "$RAM" \
-  -hda "$DISK_FILE" \
+  -m 4096 \
+  -hda freebsd.qcow2 \
   -netdev user,id=n1,hostfwd=tcp::2222-:22 \
   -device e1000,netdev=n1 \
-  -smp "$CPUS" \
+  -smp 4 \
   -cpu host
 
