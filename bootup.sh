8e069455c81587d3233ee766b7a5b1bacc830dcd
Shane Cardoz M <shanecardoz26@gmail.com>
2025-09-06 23:12:44 +0530

    the Change

--- bootup.sh
+++ bootup.sh
@@ -2,10 +2,10 @@
 
 qemu-system-x86_64 \
   -enable-kvm \
-  -m 4096 \
+  -m 10240 \
   -hda freebsd.qcow2 \
   -netdev user,id=n1,hostfwd=tcp::2222-:22 \
   -device e1000,netdev=n1 \
-  -smp 4 \
+  -smp 8 \
   -cpu host
 
